5cd5c873add74f24430fe1724da50f5797d23153
Boll <bollsudomaster@gmail.com>
2025-11-12 04:53:56 +0100

    Assets

---
+++ src/constants/projects.js
@@ -0,0 +1,12 @@
+import {ndi2024} from "./projects/ndi2024.js";
+import {studAdvisor} from "./projects/studAdvisor.js";
+import {trains} from "./projects/trains.js";
+import {cirad} from "./projects/cirad.js";
+import {veryBadSplit} from "./projects/veryBadSplit.js";
+import {gps} from "./projects/gps.js";
+
+const projects = [
+    ndi2024, studAdvisor, trains, cirad, veryBadSplit, gps
+];
+
+export { projects };
