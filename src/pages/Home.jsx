8bd605de6e2abd7d784978ba0c5d154eddf39a1a
Boll <bollsudomaster@gmail.com>
2025-11-12 05:16:17 +0100

    Home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import Hero from "../sections/Hero.jsx";
+import Me from "../sections/Me.jsx";
+import Carrier from "../sections/Carrier.jsx";
+import Projects from "../sections/Projects.jsx";
+import Contact from "../sections/Contact.jsx";
+
+const Home = () => {
+    return (
+        <>
+            <Hero />
+            <Me />
+            <Carrier />
+            <Projects />
+            <Contact />
+        </>
+    );
+};
+
+export default Home;
