1ff80f07f70895488317bac342389230f8ec75c3
Boll <bollsudomaster@gmail.com>
2026-01-11 23:25:02 +0100

    Add CV in socials and socials in hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import Cosmos from "../components/Cosmos.jsx";
 import {ArrowDownTag} from "iconoir-react";
+import Socials from "@/components/Socials.jsx";
 
 const Hero = () => {
     return (
         <section className="min-h-screen w-full flex flex-col relative">
-            <div className="w-full mx-auto flex flex-col sm:mt-26 mt-20 c-space gap-3">
-                <p className="sm:text-3xl text-2xl font-medium text-white text-center font-family-generalsans">Julien RENAUD</p>
+            <div className="w-full mx-auto items-center flex flex-col sm:mt-26 mt-20 c-space gap-3">
+                <p className="sm:text-3xl text-2xl text-secondary font-bold text-center font-family-generalsans">Julien RENAUD</p>
                 <p className="hero_tag text-gray_gradient text-center">Développeur Junior Fullstack</p>
+                <Socials />
             </div>
             
             <div className="w-full h-full absolute inset-0">
@@ -15,7 +17,7 @@ const Hero = () => {
             </div>
 
             <div className="w-full mx-auto flex flex-col justify-center items-center gap-3 absolute bottom-1">
-                <p className="hero_tag text-gray_gradient text-center">生き甲斐 Ikigai - 改善 Kaizen - 腹八分目 Hara Hachi Bu - 初心 Shoshin - 桜梅桃李 Oubaitori - 侘寂 Wabi-Sabi</p>
+                <p className="hero_tag text-gray_gradient text-center animate-glow">生き甲斐 Ikigai - 改善 Kaizen - 腹八分目 Hara Hachi Bu - 初心 Shoshin - 桜梅桃李 Oubaitori - 侘寂 Wabi-Sabi</p>
                 <button className="">
                     <a href="/#about" className="">
                         <ArrowDownTag width="3em" height="3em"/>
